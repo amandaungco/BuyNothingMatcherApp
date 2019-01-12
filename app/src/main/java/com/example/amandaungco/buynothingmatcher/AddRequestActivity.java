eddf85dfcf3a197f34a92dc19e4f3b2ea4ee008b
Amanda Ungco <amanda.ungco@gmail.com>
2019-01-12 14:41:56 -0800

    create hashmap of request form information

--- app/src/main/java/com/example/amandaungco/buynothingmatcher/AddRequestActivity.java
+++ app/src/main/java/com/example/amandaungco/buynothingmatcher/AddRequestActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.util.HashMap;
+
 public class AddRequestActivity extends AppCompatActivity {
 
     Spinner categorySpinner;
@@ -16,7 +18,6 @@ public class AddRequestActivity extends AppCompatActivity {
     EditText requestTitleField;
     EditText commentField;
     Button addRequestButton;
-
 
 
     private static final String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
@@ -28,13 +29,13 @@ public class AddRequestActivity extends AppCompatActivity {
 
         categorySpinner = findViewById(R.id.requestCategory);
         quantitySpinner = findViewById(R.id.requestQuantity);
-        requestTitleField =  findViewById(R.id.EditTextRequestTitle);
+        requestTitleField = findViewById(R.id.EditTextRequestTitle);
         commentField = findViewById(R.id.EditTextComment);
 
         categorySpinner.setAdapter(new ArrayAdapter<Category>(this,
                 android.R.layout.simple_spinner_item, Category.values()));
 
-        String[] arraySpinner = new String[] {
+        String[] arraySpinner = new String[]{
                 "1", "2", "3", "4", "5", "6", "7"
         };
 
@@ -47,19 +48,38 @@ public class AddRequestActivity extends AppCompatActivity {
 
         addRequestButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                createRequest();
+                createRequest();// create hashmap with data from form, have data sent to API
+                //with post request, pull up next activtiy and have the activity make the get
+                //request from the API to show new object
 
             }
 
 
+        };
     }
-        private Request createRequest(){
-            String requestTitle = requestTitleField.getText().toString();
-            String requestCategory = categorySpinner.getSelectedItem().toString();
-            int requestQuantity = Integer.parseInt(quantitySpinner.getSelectedItem().toString());
-            String requestComment = commentField.getText().toString();
-        }
+
+    private HashMap createRequest() {
+
+        String requestTitle = requestTitleField.getText().toString();
+        String requestCategory = categorySpinner.getSelectedItem().toString();
+        int requestQuantity = Integer.parseInt(quantitySpinner.getSelectedItem().toString());
+        String requestComment = commentField.getText().toString();
+
+        HashMap<String, String> newRequest = new HashMap<>();
+
+        // Put three keys with values.
+        newRequest.put("title", requestTitle);
+        newRequest.put("category", requestCategory);
+        newRequest.put("quantity", requestQuantity); // do i have to convert this to stirng or can it stay an integer
+
+        newRequest.put("comment", requestComment);
+
+        return newRequest;
 
     }
 
-        }
+}
+
+    private HashMap sendNewRequestToAPI(){
+
+    }
